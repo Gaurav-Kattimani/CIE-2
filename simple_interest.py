228f22a304966568227ee4784b85d221418ed1b9
Gaurav Kattimani <gaurav.kat024@gmail.com>
2025-11-21 11:43:23 +0530

    Refactor simple interest calculation with defaults
    Refactored the script to use default values for principal, rate, and time when insufficient arguments are provided. Updated the calculation and output of simple interest accordingly.

--- simple_interest.py
+++ simple_interest.py
@@ -1,13 +1,16 @@
 import sys
 
-sys.argv = ["simple_interest.py", "10000", "5", "2"]
-if len(sys.argv) != 4:
-    print("Usage: python simple_interest.py <principal> <rate> <time>")
-    sys.exit(1)
-    
-else:
+if len(sys.argv) < 4:
     principal = float(sys.argv[1])
     rate = float(sys.argv[2])
     time = float(sys.argv[3])
-    simple_interest = (principal * rate * time) / 100
-    print("The Simple Interest is:", simple_interest)
+    print("Usage : python simple_interest.py <principal> <rate> <time>")
+    sys.exit(1)
+
+else:
+    int principal, time, rate, si
+    principal=10000
+    rate=8
+    time=2
+    si = (principal * rate * time) / 100
+    print("The Simple Interest is:", si)
