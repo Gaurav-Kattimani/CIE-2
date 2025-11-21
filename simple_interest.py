149dede33e0b0ea7350653a04aced73081a44bc6
Gaurav Kattimani <gaurav.kat024@gmail.com>
2025-11-21 11:22:03 +0530

    Correct argument validation in simple_interest.py
    Fix argument check to ensure at least three arguments are provided.

--- simple_interest.py
+++ simple_interest.py
@@ -1,13 +1,13 @@
 import sys
 
-if len(sys.argv) != 4:
+if len(sys.argv) < 4:
     principal = float(sys.argv[1])
     rate = float(sys.argv[2])
     time = float(sys.argv[3])
     print("Usage : python simple_interest.py <principal> <rate> <time>")
     sys.exit(1)
 else:
-    int principal,time,rate,si;
+    int principal,time,rate,si
     principal=10000
     rate=8
     time=2
