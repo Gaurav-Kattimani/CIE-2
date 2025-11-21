c1d67f2ff7f90dccbf3877b70a00a6c4111d03c4
Gaurav Kattimani <gaurav.kat024@gmail.com>
2025-11-21 12:24:07 +0530

    Fix simple interest calculation and usage message

--- simple_interest.py
+++ simple_interest.py
@@ -4,12 +4,9 @@ if len(sys.argv) != 4:
     principal = float(sys.argv[1])
     rate = float(sys.argv[2])
     time = float(sys.argv[3])
-    print("Usage : python simple_interest.py <principal> <rate> <time>")
-    sys.exit(1)
-
 else:
     principal=10000
     rate=8
     time=2
-    si = (principal * rate * time) / 100
+si = (principal * rate * time) / 100
 print("The Simple Interest is:", si)
