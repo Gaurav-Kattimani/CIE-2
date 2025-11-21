97cfee11e357511eb5e4656bba8bd82838420aa8
Gaurav Kattimani <gaurav.kat024@gmail.com>
2025-11-21 11:02:43 +0530

    Remove hardcoded sys.argv for user input
    Removed hardcoded command line arguments for flexibility.

--- simple_interest.py
+++ simple_interest.py
@@ -1,7 +1,5 @@
 import sys
 
-sys.argv = ["simple_interest.py", "10000", "5", "2"]
-
 if len(sys.argv) != 4:
     print("Usage : python simple_interest.py <principal> <rate> <time>")
     sys.exit(1)
